4d27b6b3674979acc43093a72a853c7e613c76a1
cythreal <cythreal@gmail.com>
2025-03-28 20:11:27 -0400

    learned about sys.argv; completed CH3, L2

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 from stats import get_num_words, get_char_count, sort_dict
+import sys
 
-def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    char_dict = get_char_count(text)
-    sorted_dict = sort_dict(char_dict)
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for dict in sorted_dict:
-        if dict["name"].isalpha() == True:
-            print(f"{dict["name"]}: {dict["num"]}")
-    print("============= END ===============")
-    #print(char_dict)
-    #print(sorted_dict)
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    def main():
+        book_path = str(sys.argv[1])
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        char_dict = get_char_count(text)
+        sorted_dict = sort_dict(char_dict)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for dict in sorted_dict:
+            if dict["name"].isalpha() == True:
+                print(f"{dict["name"]}: {dict["num"]}")
+        print("============= END ===============")
+        #print(char_dict)
+        #print(sorted_dict)
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
+    def get_book_text(path):
+        with open(path) as f:
+            return f.read()
 
-main()
+    main()
